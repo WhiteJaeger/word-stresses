8cc8f0543e4fff556dabdc74f7ee7c8fe76b3a2c
Adnrei K <logerk3@gmail.com>
2022-08-06 16:05:28 +0300

    enable cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from flask import Flask, json
+from flask_cors import CORS, cross_origin
 
 from app.constants import WORDS
 
 APP = Flask(__name__)
+cors = CORS(APP)
+APP.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @APP.route('/')
@@ -11,6 +14,7 @@ def home_view():
 
 
 @APP.route('/get_word_stress/<string:word>')
+@cross_origin()
 def get_word_stress(word: str):
     return json.dumps({
         'word': word,
